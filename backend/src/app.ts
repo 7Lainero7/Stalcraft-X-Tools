8d36c7323dd9368d507ab939b0317f79ec5bb721
Lainero <7Lainero7@gmail.com>
2025-07-15 23:15:59 +0500

    added artefact routing to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import authRouter from './routes/auth.route';
 import userRouter from './routes/user.route';
 import armorRouter from './routes/armor.route';
 import containerRouter from './routes/container.route';
+import artefactRouter from './routes/artefact.route';
 import express from 'express';
 import cors from 'cors';
 
@@ -17,7 +18,7 @@ app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/armor', armorRouter);
 app.use('/api/containers', containerRouter);
-
+app.use('/api/artefacts', artefactRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`API ready on http://localhost:${PORT}`));
